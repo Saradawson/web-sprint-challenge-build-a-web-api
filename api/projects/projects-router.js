5d13714c53e2feec27b77c08f82cba6199d48419
unknown <saradawsone@gmail.com>
2022-10-18 17:11:00 -0600

    projects get request is working

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -6,4 +6,19 @@ const Projects = require('./projects-model');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    Projects.get()
+            .then(projects => {
+                res.json(projects)
+            })
+            .catch(next)
+})
+
+router.use((error, req, res, next) => { //eslint-disable-line
+    res.status(error.status || 500).json({
+        message: error.message,
+        customMessage: "Something went wrong in the project router"
+    })
+})
+
 module.exports = router;
