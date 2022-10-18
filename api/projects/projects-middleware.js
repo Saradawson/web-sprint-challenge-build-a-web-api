42bb81dd17feabfd45dbc97b775ee3c6989e5487
unknown <saradawsone@gmail.com>
2022-10-18 16:12:32 -0600

    validateProjectId middleware code written

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,24 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model');
+
+async function validateProjectId(req, res, next) {
+    try {
+        const project = await Projects.get(req.params.id)
+        if(!project){
+            res.status(404).json({
+                message: `User with id: ${req.params.id} not found`
+            })
+        }else {
+            req.porject = project;
+            next();
+        }
+    }catch(err){
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+module.exports = {
+    validateProjectId
+}
