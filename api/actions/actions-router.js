06df58d4e82fb722823c80c0fd5b0687542931e7
unknown <saradawsone@gmail.com>
2022-10-19 12:40:51 -0600

    get actions working in actoins router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,7 +4,15 @@ const Actions = require('./actions-model');
 
 const router = express.Router();
 
-router.use((error, req, res, next) => {
+router.get('/', (req, res, next) => {
+    Actions.get()
+           .then(actions => {
+            res.json(actions)
+           })
+           .catch(next)
+})
+
+router.use((error, req, res, next) => { //eslint-disable-line
     res.status(error.status || 500).json({
         message: error.message,
         customMessage: "Something went wrong in the actions router"
